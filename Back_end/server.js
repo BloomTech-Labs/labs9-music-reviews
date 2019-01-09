3a21c51ead7133fc38676d1b9cac4c309bf22058
Will Kwon <kwontaewoongah@gmail.com>
2019-01-09 14:33:07 -0800

    made user mvc

--- Back_end/server.js
+++ Back_end/server.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const server = express();
-const cors = require('cors');
+const express = require ('express');
+const server = express ();
+const cors = require ('cors');
 
-server.use(express.json());
-server.use(cors());
+server.use (express.json ());
+server.use (cors ());
 
-const reviewRoutes = require('./routes/reviewRoutes');
-
+const reviewRoutes = require ('./routes/reviewRoutes');
+const userRoutes = require ('./routes/userRoutes.js');
 // reviews route
-server.use("/reviews", reviewRoutes);
+server.use ('/reviews', reviewRoutes);
 
-const port = 9000
-server.listen(port, () => { console.log(`===Server listening on port ${port}===`) });
+// user route
+server.use ('/user', userRoutes);
 
-
+const port = 9000;
+server.listen (port, () => {
+  console.log (`===Server listening on port ${port}===`);
+});
