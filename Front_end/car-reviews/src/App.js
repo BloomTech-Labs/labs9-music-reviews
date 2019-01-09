b7dcc04240601a6e26a5f07aa5c29325bfcbdf53
DasGMA <dasgrigoma@gmail.com>
2019-01-09 02:57:44 -0800

    Presentational selet search options with no functionality

--- Front_end/car-reviews/src/App.js
+++ Front_end/car-reviews/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation';
+import Search from './Components/Search';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Search />
       </div>
     );
   }
---
+++ Front_end/car-reviews/src/Components/Search.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import { Row, Col, Button, Input} from 'react-materialize';
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            data: []
+         }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() { 
+        return (                    
+                   <Row className = 'center'>
+                   <Input s = {3} label = 'Reviewer' type = 'select'>
+                        <option value='' disabled selected>Choose reviewer (optional)</option>
+                        <option value='1'>Adam Lee</option>
+                        <option value='2'>Das MA</option>
+                        <option value='3'>Francis Tse</option>
+                        <option value='4'>AD Farris</option>
+                        <option value='5'>Will Kwon</option>
+                   </Input>
+
+                   <Input s = {2} label = 'Year' type = 'select'>
+                        <option value='' disabled selected>Choose year</option>
+                        <option value='1'>Option 1</option>
+                        <option value='2'>Option 2</option>
+                        <option value='3'>Option 3</option>
+                   </Input>
+
+                   <Input s = {3} label = 'Make' type = 'select'>
+                        <option value='' disabled selected>Choose make</option>
+                        <option value='1'>Option 1</option>
+                        <option value='2'>Option 2</option>
+                        <option value='3'>Option 3</option>
+                   </Input>
+
+                   <Input s = {2} label = 'Model' type = 'select'>
+                        <option value='' disabled selected>Choose model</option>
+                        <option value='1'>Option 1</option>
+                        <option value='2'>Option 2</option>
+                        <option value='3'>Option 3</option>
+                   </Input>
+
+                   <Input s = {2} label = 'Trim' type = 'select'>
+                        <option value='' disabled selected>Choose trim</option>
+                        <option value='1'>Option 1</option>
+                        <option value='2'>Option 2</option>
+                        <option value='3'>Option 3</option>
+                   </Input>
+
+                       <Col s={12} >
+                            <Button>Review</Button>
+                            <Button>Search</Button>
+                       </Col>
+                   </Row> 
+         );
+    }
+}
+ 
+export default Search;
