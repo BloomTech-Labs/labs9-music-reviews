ec2fca20c4c15c82c11c770f725135144f9cdcf5
DasGMA <dasgrigoma@gmail.com>
2019-01-09 07:37:04 -0800

    new stars rating

--- Front_end/car-reviews/src/Components/Featured_reviews.js
+++ Front_end/car-reviews/src/Components/Featured_reviews.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Section, Row, Col, Card, CardTitle} from 'react-materialize';
+import Stars from './Stars';
 
 
 class FeaturedReviews extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+
+         }
     }
     render() { 
         return (
@@ -13,30 +16,34 @@ class FeaturedReviews extends Component {
                 <h1>Featured Reviews</h1>
                 <Row>
                 <Col m={6} s={12} l={3}>
-                    <Card className='small'
+                    <Card className='medium'
                         header={<CardTitle image='img/sample-1.jpg'></CardTitle>}
                         actions={[<a href='#'>@reviewer</a>]}>
+                        <div><Stars /></div>
                         2006 Mercedes-Benz GL450
                     </Card>
                 </Col>
                 <Col m={6} s={12} l={3}>
-                    <Card className='small'
+                    <Card className='medium'
                         header={<CardTitle image='img/sample-1.jpg'></CardTitle>}
                         actions={[<a href='#'>@reviewer</a>]}>
+                        <div><Stars /></div>
                         2018 Toyota Rav4 XLE
                     </Card>
                 </Col>
                 <Col m={6} s={12} l={3}>
-                    <Card className='small'
+                    <Card className='medium'
                         header={<CardTitle image='img/sample-1.jpg'></CardTitle>}
                         actions={[<a href='#'>@reviewer</a>]}>
+                        <div><Stars /></div>
                         2006 Toyota Prius Hybrid
                     </Card>
                 </Col>
                 <Col m={6} s={12} l={3}>
-                    <Card className='small'
+                    <Card className='medium'
                         header={<CardTitle image='img/sample-1.jpg'></CardTitle>}
                         actions={[<a href='#'>@reviewer</a>]}>
+                        <div><Stars /></div>
                         2006 Hyundai Elantra GL
                     </Card>
                 </Col>
