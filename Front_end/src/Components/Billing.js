96515fbc4d1d9a48efb463a840b26a917350020b
francistse23 <wtse.ucsd@gmail.com>
2019-01-10 15:33:28 -0800

    Fixed styling for Billing page. Still need to work on styling for different screen sizes.

--- Front_end/src/Components/Billing.js
+++ Front_end/src/Components/Billing.js
@@ -1,56 +1,69 @@
 import React, { Component } from 'react';
-import { SideNav, SideNavItem, Breadcrumb, MenuItem, Card, CardTitle, Button, Row, Input } from 'react-materialize';
+import { SideNav, SideNavItem, Breadcrumb, MenuItem, Card, Button, Row, Col, Input } from 'react-materialize';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const Payment = styled.div`
-    padding-left: 3rem;
+    padding: 3rem;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    height: 80vh;
-`
+    align-content: space-between;
+`;
 
 class Billing extends Component {
     render(){
         return (
             <div>
-                <Breadcrumb style={{padding:"0"}}>
+                <Breadcrumb>
                     <MenuItem>
                     <a href="/">Home</a>
                     </MenuItem>
                     <MenuItem>Billing</MenuItem>
                 </Breadcrumb>
 
-                
-                <div style={{ display:"flex"}}>
+                <Row>
+                    <div class="col s2">
+                        <SideNav
+                            trigger={<Button>SIDE NAV</Button>}
+                            options={{ closeOnClick: true }}
+                        >
+                            <SideNavItem userView
+                                user={{
+                                    background: 'img/office.jpg',
+                                    image: 'img/yuna.jpg',
+                                    name: 'John Doe',
+                                    email: 'jdandturk@gmail.com'
+                                }}
+                            />
+                            <SideNavItem icon="search">Search</SideNavItem>
+                            <SideNavItem icon="rate_review">My Reviews</SideNavItem>
+                            <SideNavItem icon="attach_money">
+                                <Link to="/billing">Billing</Link>
+                            </SideNavItem>
+                            <SideNavItem icon="settings">Settings</SideNavItem>
+                            <SideNavItem divider />
+                            <SideNavItem icon="cancel">Sign Out</SideNavItem>
+                        </SideNav>
+                    </div>
 
-                    <SideNav style={{ position: "relative", height: "95vh" }}>
-                        <SideNavItem icon="search">Search</SideNavItem>
-                        <SideNavItem icon="rate_review">My Reviews</SideNavItem>
-                        <SideNavItem icon="attach_money">
-                            <Link to="/billing">Billing</Link>
-                        </SideNavItem>
-                        <SideNavItem icon="settings">Settings</SideNavItem>
-                        <SideNavItem divider />
-                        <SideNavItem icon="cancel">Sign Out</SideNavItem>
-                    </SideNav>
-
-                    <Payment>
-                        <h3> Billing </h3>
-                        <Card title="Payment Info">
-                            <Row style={{width: "40%"}}>
-                                <Input placeholder="Credit Card #" />
-                                <Input placeholder="Expiration Date" />
+                    <div class="valign-wrapper s10">
+                        <Payment>
+                            <h2> Billing </h2>
+                            <Card title="Payment Info" class="large" style={{ padding: "3rem 10rem 3rem 0", textAlign: "left" }}>
+                                <Input placeholder="Credit Card #" /><br />
+                                <Input placeholder="Expiration Date" /><br />
                                 <Input placeholder="CVV" />
-                            </Row>
-                        </Card>
-                        <Input name='year' type='checkbox' value='1 Year Subscription - $ 9.99' label='1 Year Subscription - $ 9.99' />
-                        <Input name='month' type='checkbox' value='1 Month Subscription - $ 0.99' label='1 Month Subscription - $ 0.99' />
-                        <Button waves='light' large={true}>Buy Now</Button>
-                    </Payment>
+                            </Card>
+                            <div style={{ margin: "2rem 0" }}>
+                                <Input name='year' type='checkbox' value='1 Year Subscription - $ 9.99' label='1 Year Subscription - $ 9.99' /><br /><br />
+                                <Input name='month' type='checkbox' value='1 Month Subscription - $ 0.99' label='1 Month Subscription - $ 0.99' />
+                            </div>
+                            <Button waves='light' large={true}>Buy Now</Button>
+                        </Payment>            
+                    </div>
+                </Row>
 
-                </div>
             </div>
         )
     };
