8d20f434d736e133352ea1dd4e84144f9050caac
adamcglee <adamcglee@gmail.com>
2019-01-09 16:59:23 -0800

    added specific modal components to help make code DRY and made some layout changes

--- Front_end/src/Components/reviewlist.js
+++ Front_end/src/Components/reviewlist.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
+import NewReviewModalCard from "./NewReviewModalCard";
+import EditReviewModalCard from "./EditReviewModalCard";
 import {
   Button,
   Modal,
@@ -10,24 +12,34 @@ import {
   Dropdown,
   SideNav,
   SideNavItem,
-  Icon
+  Col,
+  Row,
+  Card,
+  CardTitle
 } from "react-materialize";
+
+const BreadcrumbDiv = styled.div`
+  text-align: left;
+`;
 
 const ReviewList = props => {
   return (
     <div>
-      {/* Breadcrumb Nav */}
-      <Breadcrumb>
-        <MenuItem>
-          <a href="/">Home</a>
-        </MenuItem>
-        <MenuItem>My Reviews</MenuItem>
-      </Breadcrumb>
-      <Navbar right>
-        <NavItem href="/">Sign Out</NavItem>
-      </Navbar>
+      <BreadcrumbDiv>
+        {/* Breadcrumb*/}
+        <Breadcrumb>
+          <MenuItem>
+            <a href="/">Home</a>
+          </MenuItem>
+          <MenuItem>My Reviews</MenuItem>
+        </Breadcrumb>
+      </BreadcrumbDiv>
+
+      {/* If no reviews on account display prompt for new review otherwise display all reviews on account */}
+
       <div>
-          <SideNav>
+        <div>
+          {/* <SideNav fixed="false">
             <SideNavItem
               userView
               user={{
@@ -43,43 +55,24 @@ const ReviewList = props => {
             <SideNavItem icon="settings">Settings</SideNavItem>
             <SideNavItem divider />
             <SideNavItem icon="cancel">Sign Out</SideNavItem>
-          </SideNav>
+          </SideNav> */}
+        </div>
+        <div>
+          {1 === 0 ? (
+            <div>
+              <NewReviewModalCard />
+            </div>
+          ) : (
+            <div>
+              {/* Review Card Map */}
+              <Row>
+                <EditReviewModalCard />
+                <NewReviewModalCard />
+              </Row>
+            </div>
+          )}
+        </div>
       </div>
-      {/* If no reviews on account display prompt for new review otherwise display all reviews on account */}
-      {/* {props.reviews.length === 0 ? ( */}
-      <div>
-        <h4>Add a new review.</h4>
-        <Button floating large className="red" waves="light" icon="add" />
-      </div>
-      {/* ) : ( */}
-      <div>
-        <Modal
-          header="Year Make Model Trim"
-          trigger={<Button>MODAL</Button>}
-          width="250 px"
-        >
-          <p>Review by: @name</p>
-          <img
-            src="https://mygoto.io/assets/web/images/placeholder-img.jpg"
-            alt=""
-          />
-          <p>Stars</p>
-          <Dropdown trigger={<Button>Select One</Button>}>
-            <NavItem>Owned</NavItem>
-            <NavItem>Rented</NavItem>
-            <NavItem>Driven</NavItem>
-          </Dropdown>
-          <textarea
-            type="textarea"
-            placeholder="Type Review"
-            //   onChange={props.handleReviewChange}
-            //   value={props.reviews.review}
-            name="review"
-          />
-        </Modal>
-        Reviews are here!
-      </div>
-      {/* )} */}
     </div>
   );
 };
