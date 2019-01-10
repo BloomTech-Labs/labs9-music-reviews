bab5150a976e18aca43f349e5662f4c4f9dc87b8
Will Kwon <kwontaewoongah@gmail.com>
2019-01-09 19:53:04 -0800

    made LogInModal component no functionality yet

--- Front_end/src/Components/Navigation.js
+++ Front_end/src/Components/Navigation.js
@@ -1,47 +1,65 @@
 import React, {Component} from 'react';
-import {Navbar, NavItem, SideNav, SideNavItem, Icon, Button} from 'react-materialize';
+import LogInModal from './LogInModal.js';
+
+import {
+  Navbar,
+  NavItem,
+  SideNav,
+  SideNavItem,
+  Icon,
+  Button,
+} from 'react-materialize';
 
 class Navigation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            isLoggedIn: false
-         }
-    }
-    render() {
-        const LoggedIn = <Navbar className = 'blue' right>
-                            <SideNav
-                                trigger={<Button style = {{background: 'inherit', hover: 'none', border: 'none'}}><Icon large>menu</Icon></Button>}
-                                options={{ closeOnClick: true }}
-                                >
-                                <SideNavItem userView
-                                    user={{
-                                    background: 'img/image.jpg',
-                                    image: 'img/image.jpg',
-                                    name: 'Name Surname',
-                                    email: 'gmailk@gmail.com'
-                                    }}
-                                />
-                                <SideNavItem icon = 'search'>Search</SideNavItem>
-                                <SideNavItem icon = 'rate_review'>My Reviews</SideNavItem>
-                                <SideNavItem icon = 'attach_money'>Billing</SideNavItem>
-                                <SideNavItem icon = 'settings'>Settings</SideNavItem>
-                                <SideNavItem divider />
-                                <SideNavItem icon = 'cancel'>Sign Out</SideNavItem>
-                            </SideNav>
-                        </Navbar>
+  constructor (props) {
+    super (props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+  render () {
+    const LoggedIn = (
+      <Navbar className="blue" right>
+        <SideNav
+          trigger={
+            <Button
+              style={{background: 'inherit', hover: 'none', border: 'none'}}
+            >
+              <Icon large>menu</Icon>
+            </Button>
+          }
+          options={{closeOnClick: true}}
+        >
+          <SideNavItem
+            userView
+            user={{
+              background: 'img/image.jpg',
+              image: 'img/image.jpg',
+              name: 'Name Surname',
+              email: 'gmailk@gmail.com',
+            }}
+          />
+          <SideNavItem icon="search">Search</SideNavItem>
+          <SideNavItem icon="rate_review">My Reviews</SideNavItem>
+          <SideNavItem icon="attach_money">Billing</SideNavItem>
+          <SideNavItem icon="settings">Settings</SideNavItem>
+          <SideNavItem divider />
+          <SideNavItem icon="cancel">Sign Out</SideNavItem>
+        </SideNav>
+      </Navbar>
+    );
 
-        const LoggedOut = <Navbar className = 'blue' right>
-                            <NavItem>Sign Up</NavItem>
-                            <NavItem>Sign In</NavItem>
-                        </Navbar>
+    const LoggedOut = (
+      <Navbar className="blue" right>
+        <LogInModal />
+        <NavItem>Sign In</NavItem>
+      </Navbar>
+    );
 
-        const {isLoggedIn} = this.state;
+    const {isLoggedIn} = this.state;
 
-        return ( 
-                isLoggedIn ? LoggedIn : LoggedOut
-         );
-    }
+    return isLoggedIn ? LoggedIn : LoggedOut;
+  }
 }
- 
+
 export default Navigation;
