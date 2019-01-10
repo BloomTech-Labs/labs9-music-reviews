bd9e44a84382110403127a6b659f96361bd790d3
DasGMA <dasgrigoma@gmail.com>
2019-01-10 10:19:00 -0800

    Naming updates

--- Front_end/src/App.js
+++ Front_end/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import ReviewList from './Components/reviewlist'
 import { Route } from 'react-router-dom'
-import LandingPage from './Components/Landing_page'
-import Search_Landing from './Components/Search_Landing'
+import LandingPage from './Components/LandingPage'
+import SearchLanding from './Components/SearchLanding'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path="/" component={LandingPage} />
-        <Route path="/search_landing" component={Search_Landing} />
+        <Route path="/search_landing" component={SearchLanding} />
         <Route path="/reviews" component={ReviewList} />
       </div>
     )
