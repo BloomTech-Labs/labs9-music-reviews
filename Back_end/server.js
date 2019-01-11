728bcfebce5223b93bbf300945af4ae8e475d121
A.D.Faris <adfaris@gmail.com>
2019-01-11 13:54:26 -0500

    added sanity check at the root

--- Back_end/server.js
+++ Back_end/server.js
@@ -1,19 +1,25 @@
-const express = require ('express');
-const server = express ();
-const cors = require ('cors');
+const express = require('express')
+const server = express()
+const cors = require('cors')
 
-server.use (express.json ());
-server.use (cors ());
+server.use(express.json())
+server.use(cors())
 
-const reviewRoutes = require ('./routes/reviewRoutes');
-const userRoutes = require ('./routes/userRoutes.js');
+const reviewRoutes = require('./routes/reviewRoutes')
+const userRoutes = require('./routes/userRoutes.js')
+
+// sanity check
+server.use('/', (req, res) => {
+  res.send('Server running...')
+})
+
 // reviews route
-server.use ('/reviews', reviewRoutes);
+server.use('/reviews', reviewRoutes)
 
 // user route
-server.use ('/user', userRoutes);
+server.use('/user', userRoutes)
 
-const port = 9000;
-server.listen (port, () => {
-  console.log (`===Server listening on port ${port}===`);
-});
+const port = 9000
+server.listen(port, () => {
+  console.log(`===Server listening on port ${port}===`)
+})
