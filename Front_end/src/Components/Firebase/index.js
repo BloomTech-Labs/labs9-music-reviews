33c8cbd6b150330b53a86be4f64c06c2d06174e1
Will Kwon <kwontaewoongah@gmail.com>
2019-01-12 20:25:37 -0800

    wrapped app component in index.js with FirebaseContext.Provider

---
+++ Front_end/src/Components/Firebase/index.js
@@ -0,0 +1,8 @@
+/*file exports all necessary functionalities 
+of firebase (firebase class and firebase context 
+for consumer and provider components)*/
+import FirebaseContext from './context';
+import Firebase from './firebase';
+
+export default Firebase;
+export {FirebaseContext};
--- Front_end/src/index.js
+++ Front_end/src/index.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-import { BrowserRouter as Router } from 'react-router-dom'
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Firebase, {FirebaseContext} from './components/Firebase';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root'),
-)
+ReactDOM.render (
+  <FirebaseContext.Provider value={new Firebase ()}>
+    <Router>
+      <App />
+    </Router>
+  </FirebaseContext.Provider>,
+  document.getElementById ('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister ();
