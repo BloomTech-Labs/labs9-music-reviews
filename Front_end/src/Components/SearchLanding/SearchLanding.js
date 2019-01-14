4a95a40d0e4d08a6044d11f53f7d80a3ca348458
francistse23 <wtse.ucsd@gmail.com>
2019-01-14 11:59:02 -0800

    More import changes due to folders/components restructure.

--- Front_end/src/Components/SearchLanding/SearchLanding.js
+++ Front_end/src/Components/SearchLanding/SearchLanding.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navigation from './Navigation'
+import Navigation from '../Navigation/Navigation'
 import Search from '../SearchLanding/Search'
 import Filter from '../SearchLanding/Filter'
 import SearchView from '../SearchLanding/SearchView'
