203e198bd142f70eafc6e5e61c1319a7b7981eaf
Will Kwon <kwontaewoongah@gmail.com>
2019-01-17 09:20:19 -0800

    changed firebase configurations

--- Back_end/firebaseSDK/index.js
+++ Back_end/firebaseSDK/index.js
@@ -1,7 +1,10 @@
-var admin = require('firebase-admin');
-var serviceAccount = require('../serviceAccountKey.json');
+var admin = require ('firebase-admin');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+admin.initializeApp ({
+  credential: admin.credential.cert ({
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace (/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 module.exports = admin;
