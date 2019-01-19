a43d7829cf6baf3cbb4a79f19b95f67966a23797
A.D.Faris <adfaris@gmail.com>
2019-01-19 17:29:51 -0500

    some faker data

--- Back_end/migrations/20190119122917_musicReviewTables.js
+++ Back_end/migrations/20190119122917_musicReviewTables.js
@@ -1,34 +1,48 @@
-exports.up = function (knex, Promise) {
+exports.up = function(knex, Promise) {
   return knex.schema
-    .createTable('users', function (usr) {
+    .createTable('users', function(usr) {
       usr.increments('userID').primary()
       usr.string('emailAddress', 100)
       usr.boolean('paidMembership').defaultTo('false')
       usr.integer('reviewCount')
       usr.datetime('subscriptionExpiration')
-      usr.string('nickName').unique()
+      usr.string('nickName')
     })
-    .createTable('albumReview', function (alb) {
+    .createTable('albumReview', function(alb) {
       alb.increments('albumID').primary()
       alb.datetime('dateCreated')
       alb.datetime('dateModified')
       alb.integer('rating')
       alb.text('review')
       alb.string('spotifyAlbumID')
-      alb.integer('userID').unsigned().notNullable().references('userID').inTable('users').onDelete('cascade').index()
+      alb
+        .integer('userID')
+        .unsigned()
+        .notNullable()
+        .references('userID')
+        .inTable('users')
+        .onDelete('cascade')
+        .index()
     })
-    .createTable('trackReview', function (trk) {
+    .createTable('trackReview', function(trk) {
       trk.increments('reviewID').primary()
       trk.datetime('dateCreated')
       trk.datetime('dateModified')
       trk.integer('rating')
       trk.text('review')
       trk.string('spotifyTrackID')
-      trk.integer('userID').unsigned().notNullable().references('userID').inTable('users').onDelete('cascade').index()
+      trk
+        .integer('userID')
+        .unsigned()
+        .notNullable()
+        .references('userID')
+        .inTable('users')
+        .onDelete('cascade')
+        .index()
     })
 }
 
-exports.down = function (knex, Promise) {
+exports.down = function(knex, Promise) {
   return knex.schema
     .dropTable('albumReview')
     .dropTable('trackReview')
--- Back_end/seeds/albumReviews.js
+++ Back_end/seeds/albumReviews.js
@@ -1,36 +1,45 @@
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('albumReview').del().then(function() {
-    // Inserts seed entries
-    return knex('albumReview').insert([
-      {
-        dateCreated: '1/18/2019',
-        dateModified: '1/18/2019',
-        rating: 3,
-        review: 'class MaxStack{constructor(){this.stack = new Stack()',
-        spotifyAlbumID: '382ObEPsp2rxGrnsizN5TX',
-      },
-      {
-        dateCreated: '1/18/2019',
-        dateModified: '1/18/2019',
-        rating: 3,
-        review: 'class MaxStack{constructor(){this.stack = new Stack()',
-        spotifyAlbumID: '382ObEPsp2rxGrnsizN5TX',
-      },
-      {
-        dateCreated: '1/18/2019',
-        dateModified: '1/18/2019',
-        rating: 3,
-        review: 'class MaxStack{constructor(){this.stack = new Stack()',
-        spotifyAlbumID: '382ObEPsp2rxGrnsizN5TX',
-      },
-      {
-        dateCreated: '1/18/2019',
-        dateModified: '1/18/2019',
-        rating: 3,
-        review: 'class MaxStack{constructor(){this.stack = new Stack()',
-        spotifyAlbumID: '382ObEPsp2rxGrnsizN5TX',
-      },
-    ])
-  })
+const faker = require('faker')
+const createFakeUser = () => ({
+  emailAddress: faker.internet.email(),
+  paidMembership: faker.random.boolean(),
+  reviewCount: faker.random.number(),
+  subscriptionExpiration: faker.random.number(),
+  nickName: faker.random.word().unique(),
+})
+const createFakeAlbumReview = () => ({
+  dateCreated: faker.random.number(),
+  dateModified: faker.random.number(),
+  rating: faker.random.number(),
+  review: faker.lorem.sentence(),
+  spotifyTrackID: faker.random.uuid(),
+})
+const createFakeTrackReview = () => ({
+  dateCreated: faker.random.number(),
+  dateModified: faker.random.number(),
+  rating: faker.random.number(),
+  review: faker.lorem.sentence(),
+  spotifyTrackID: faker.random.uuid(),
+})
+
+exports.seed = async function(knex, Promise) {
+  const fakeUsers = []
+  const desiredFakeUsers = 50
+  for (let i = 0; i < desiredFakeUsers; i++) {
+    fakeUsers.push(createFakeUser())
+  }
+  await knex('users').insert(fakeUsers)
+
+  const fakeTrackReview = []
+  const desiredFakeTrackReview = 50
+  for (let i = 0; i < desiredFakeTrackReview; i++) {
+    fakeTrackReview.push(createFakeTrackReview())
+  }
+  await knex('trackReview').insert(fakeTrackReview)
+
+  const fakeAlbumReview = []
+  const desiredFakeAlbumReview = 50
+  for (let i = 0; i < desiredFakeAlbumReview; i++) {
+    fakeTrackReview.push(createFakeAlbumReview())
+  }
+  await knex('trackReview').insert(fakeTrackReview)
 }
