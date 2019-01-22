daea29776251ebc5d5ba99efacb5042a5e8c8044
adamcglee <adamcglee@gmail.com>
2019-01-22 13:08:21 -0800

    added some framework for user id dynamic routing and added a modal for billing subscription information.

--- Front_end/src/Components/ReviewList/ProfileReviewCard.js
+++ Front_end/src/Components/ReviewList/ProfileReviewCard.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import ReviewEditModal from "../CardModals/ReviewEditModal";
 import ViewStars from "../StarsRating/ViewStars";
-import { Row, Col } from "reactstrap";
+import { Row, Col, Container } from "reactstrap";
 import axios from "axios";
 import { instanceOf } from "prop-types";
 import { withCookies, Cookies } from "react-cookie";
@@ -40,19 +40,6 @@ class ProfileReviewCard extends Component {
       .catch(err => console.log(err));
   };
 
-  // getTracks = (albumId, token) => {
-  //   axios
-  //     .get(`https://api.spotify.com/v1/albums/${albumId}/tracks`, {
-  //       headers: { Authorization: `Bearer ${token}` }
-  //     })
-  //     .then(data => {
-  //       this.setState({
-  //         tracks: data.data.items
-  //       });
-  //     })
-  //     .catch(err => console.log(err));
-  // };
-
   componentDidMount() {
     this.getAlbum(
       this.props.review.spotifyAlbumID,
@@ -62,45 +49,47 @@ class ProfileReviewCard extends Component {
   render() {
     console.log(this.state.art);
     return (
-      <div>
-        <Row style={{ display: "flex", padding: "3rem 1rem" }}>
-          {/* User info */}
-          <Col md="3" style={{ margin: "auto 0" }}>
-            <img src={this.state.art} alt="Album cover art" />
-            <div>Album: {this.state.album}</div>
-            <div>Artist: {this.state.artist}</div>
-            {/* {this.props.review.trackName ? (
+      <Fragment>
+        <Container>
+          <Row style={{ display: "flex", padding: "3rem 1rem" }}>
+            {/* User info */}
+            <Col md="3" style={{ margin: "auto 0" }}>
+              <img src={this.state.art} alt="Album cover art" />
+              <div>Album: {this.state.album}</div>
+              <div>Artist: {this.state.artist}</div>
+              {/* {this.props.review.trackName ? (
               <div>{this.props.review.trackName}</div>
             ) : null} */}
-            {/* If logged in edit button shows otherwise null */}
-            {this.props.loggedIn === true ? (
-              <ReviewEditModal
-                {...this.props}
-                album={this.state.album}
-                artist={this.state.artist}
-                art={this.state.art}
-                tracks={this.state.tracks}
-              />
-            ) : null}
-          </Col>
-          <Col md="9" style={{ padding: "1rem 5rem" }}>
-            <Row style={{ display: "flex" }}>
-              <ViewStars rating={this.props.review.rating} />
-              <p style={{ padding: "0 20px" }}>
-                Date Created: {this.props.review.dateCreated}
-              </p>
-              <p style={{ padding: "0 20px" }}>
-                Updated On: {this.props.review.dateModified}
-              </p>
-            </Row>
-            <Row>
-              <div align="left">
-                <p>{this.props.review.review}</p>
-              </div>
-            </Row>
-          </Col>
-        </Row>
-      </div>
+              {/* If logged in edit button shows otherwise null */}
+              {this.props.loggedIn === true ? (
+                <ReviewEditModal
+                  {...this.props}
+                  album={this.state.album}
+                  artist={this.state.artist}
+                  art={this.state.art}
+                  tracks={this.state.tracks}
+                />
+              ) : null}
+            </Col>
+            <Col md="9" style={{ padding: "1rem 5rem" }}>
+              <Row style={{ display: "flex" }}>
+                <ViewStars rating={this.props.review.rating} />
+                <p style={{ padding: "0 20px" }}>
+                  Date Created: {this.props.review.dateCreated}
+                </p>
+                <p style={{ padding: "0 20px" }}>
+                  Updated On: {this.props.review.dateModified}
+                </p>
+              </Row>
+              <Row>
+                <div align="left">
+                  <p>{this.props.review.review}</p>
+                </div>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
     );
   }
 }
--- Front_end/src/Components/ReviewList/ReviewList.js
+++ Front_end/src/Components/ReviewList/ReviewList.js
@@ -1,17 +1,19 @@
 import React, { Component, Fragment } from "react";
 import styled from "styled-components";
 import axios from "axios";
-import { Row, Col, Container } from "reactstrap";
+import { Row, Col, Container, CardImg, Button } from "reactstrap";
 import ProfileReviewCard from "./ProfileReviewCard";
 
-const ProfileInfo = styled.div`
+const Sidebar = styled.div`
   position: -webkit-sticky;
   position: sticky;
   top: 0;
   z-index: 1;
-  overflow-x: hidden;
   height: 100%;
-  padding-top: 20px;
+  padding-top: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: "center";
 `;
 
 class ReviewList extends Component {
@@ -20,13 +22,14 @@ class ReviewList extends Component {
     this.state = {
       reviews: [],
       input: "",
-      loggedIn: true,
+      loggedIn: false,
       username: ""
     };
   }
 
   componentDidMount() {
     this.getAlbumReviews();
+    this.setState({ loggedIn: this.props.match.params.loggedIn });
   }
 
   getAlbumReviews() {
@@ -34,7 +37,9 @@ class ReviewList extends Component {
       .get("https://labs9-car-reviews.herokuapp.com/albumReviews")
       .then(response => {
         const userReviews = response.data;
-        const newState = Object.assign({}, this.state, { reviews: userReviews });
+        const newState = Object.assign({}, this.state, {
+          reviews: userReviews
+        });
         this.setState(newState);
       })
       .catch(err => console.log(err));
@@ -45,41 +50,64 @@ class ReviewList extends Component {
   };
 
   render() {
-    // const userReviews = this.state.reviews.filter(review => {
-    //   return (
-    //     review.review
-    //       .toLowerCase()
-    //       .indexOf(this.state.username.toLowerCase()) !== -1
-    //   );
-    // });
+    console.log(this.props.match);
+    console.log(this.props);
+    const userReviews = this.state.reviews.filter(review => {
+      return (
+        review.review
+          .toLowerCase()
+          .indexOf(this.state.username.toLowerCase()) !== -1
+      );
+    });
     return (
       <Fragment>
-        <div>
           <Container
             fluid={true}
             style={{
               display: "flex",
               justifyItems: "space-around",
-              margin: "0 auto"
+              margin: "0 auto",
+              maxWidth: "1600px"
             }}
           >
-            <ProfileInfo>
-              <img
+            <Sidebar>
+              <CardImg
                 src={require("../../Images/RecordThumb.png")}
                 alt="Default profile image"
-                style={{ maxWidth: '200px'}}
+                style={{ maxWidth: "200px" }}
               />
-              <p>Status</p>
-              <p>Username</p>
-              <p>Reviews: {this.state.reviews.length}</p>
-            </ProfileInfo>
+              <Row style={{ alignSelf: "center" }}>
+                <h3>Username</h3>
+              </Row>
+              <Row style={{ alignSelf: "center" }}>
+                <h5>Status</h5>
+              </Row>
+              <Row style={{ alignSelf: "center" }}>
+                <h5>Reviews: {this.state.reviews.length}</h5>
+              </Row>
+              {/* can add logic to render different size of album art based on screen size: stacked ternary */}
+              {/* need to find a way to manipulate the img object from res.data */}
+
+              <Row
+                style={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  padding: "1rem"
+                }}
+              >
+                <Button>Buy Now</Button>
+              </Row>
+            </Sidebar>
           </Container>
-          <Container fluid={true}>
-            {this.state.reviews.map(review => (
-              <ProfileReviewCard review={review} loggedIn={this.state.loggedIn} key={review.id}/>
+          <Container fluid={true} style={{ maxWidth: "1150px" }}>
+            {userReviews.map(review => (
+              <ProfileReviewCard
+                review={review}
+                loggedIn={this.state.loggedIn}
+                key={review.id}
+              />
             ))}
           </Container>
-        </div>
       </Fragment>
     );
   }
