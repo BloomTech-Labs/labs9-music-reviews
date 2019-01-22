62e310947790c87d70e87091bc4953ff1aac4e99
francistse23 <wtse.ucsd@gmail.com>
2019-01-22 08:31:56 -0800

    Minor changes since last restructure of back end routes.

--- Back_end/index.js
+++ Back_end/index.js
@@ -8,19 +8,7 @@ require('dotenv').load();
 const app = express();
 
 app.use(cors())
-<<<<<<< HEAD
   .use(express.json());
-=======
-  .use(express.json())
-  .use(express.static(__dirname + '/public'))
-  .use(cookieParser())
-  .use(session({
-    // cookie: { domain: 'https://labs9carreviews.netlify.com' }
-    secret: 'LambdaLabsCS12MusicReviews',
-    cookie: { domain: '.labs9carreviews.netlify.com' }
-  }));
-
->>>>>>> 3dc50c4b490c84a1628c2f6ae3657c91ce0cdbfb
 
 const configureRoutes = require('./routes');
 configureRoutes(app);
@@ -28,13 +16,10 @@ configureRoutes(app);
 const trackReviewRoutes = require('./routes/trackReviewRoutes.js');
 const albumReviewRoutes = require('./routes/albumReviewRoutes.js');
 const userRoutes = require('./routes/userRoutes.js');
-<<<<<<< HEAD
 const spotifyRoutes = require('./routes/spotifyRoutes');
-
-=======
 const likedAlbumReviewRoutes = require('./routes/likedAlbumReviewRoutes.js');
-const likedTrackReviewRoutes = require('./routes/likedTrackReviewRoutes.js')
->>>>>>> 3dc50c4b490c84a1628c2f6ae3657c91ce0cdbfb
+const likedTrackReviewRoutes = require('./routes/likedTrackReviewRoutes.js');
+
 // track reviews route
 app.use('/trackReviews', trackReviewRoutes);
 
