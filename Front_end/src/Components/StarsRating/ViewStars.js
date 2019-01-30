02ab3dcbadc65022e13683b783b02bd3341fce12
adamcglee <adamcglee@gmail.com>
2019-01-29 23:02:43 -0800

    updated user review list styles with responsiveness and aesthetics

--- Front_end/src/Components/StarsRating/ViewStars.js
+++ Front_end/src/Components/StarsRating/ViewStars.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
-import Ratings from 'react-ratings-declarative';
-
+import React, { Component } from "react";
+import Ratings from "react-ratings-declarative";
+import styled from "styled-components";
 
 class ViewStars extends Component {
   render() {
     return (
-      <Ratings 
-        rating={this.props.rating} 
+      <Ratings
+        rating={this.props.rating}
         widgetRatedColors="orange"
-        widgetDimensions = "30px"
-        >
+        widgetDimensions="30px"
+        widgetSpacings="2px"
+        style={{ align: "center"}}
+      >
         <Ratings.Widget />
         <Ratings.Widget />
         <Ratings.Widget />
         <Ratings.Widget />
         <Ratings.Widget />
       </Ratings>
-    )
+    );
   }
 }
 
